8e61c426b11ace99a9a59b599f9c6ba7ac849828
David James <djames@sunlightfoundation.com>
2009-12-13 15:15:11 -0500

    first version

---
+++ lib/info_unit.rb
@@ -0,0 +1,58 @@
+class InfoUnit
+  
+  attr_reader :number
+  attr_reader :unit
+  attr_reader :bytes
+  
+  def initialize(*args)
+    case args.length
+    when 1
+      parse(args[0])
+    when 2
+      @number = args[0]
+      @unit   = args[1].to_s
+    else
+      raise ArgumentError
+    end
+  end
+  
+  def bytes
+    @number * multiplier if @number
+  end
+  
+  def to_hash
+    if @unit && @number
+      { @unit => @number }
+    else
+      {}
+    end
+  end
+
+  MULTIPLIERS = {
+    'B'  => 1,
+    'KB' => 1024,
+    'MB' => 1048576,
+    'TB' => 1073741824,
+    'PB' => 1099511627776,
+    'EB' => 1125899906842624,
+  }
+  
+  SUFFIX  = "[ ]*(B|KB|MB|TB|PB|EB)$"
+  FLOAT   = Regexp.compile("^([0-9\.]+)" + SUFFIX)
+  INTEGER = Regexp.compile("^([0-9]+)" + SUFFIX)
+
+  def multiplier
+    MULTIPLIERS[@unit]
+  end
+
+  def parse(string)
+    if INTEGER.match(string)
+      @number = Regexp.last_match(1).to_i
+      @unit   = Regexp.last_match(2)
+    elsif FLOAT.match(string)
+      @number = Regexp.last_match(1).to_f
+      @unit   = Regexp.last_match(2)
+    end
+  end
+  
+end
--- spec/info_unit_spec.rb
+++ spec/info_unit_spec.rb
@@ -1,7 +1,77 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "InfoUnit" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  
+  describe "#new" do
+
+    it "100 KB" do
+      [
+        InfoUnit.new(100, 'KB'),
+        InfoUnit.new('100KB'),
+        InfoUnit.new('100 KB'),
+      ].each do |x|
+        x.number.should == 100
+        x.unit.should == 'KB'
+        x.bytes.should == 100 * 1024
+        x.to_hash.should == { 'KB' => 100 }
+      end
+    end
+  
+    it "27 MB" do
+      [
+        InfoUnit.new(27, 'MB'),
+        InfoUnit.new('27MB'),
+        InfoUnit.new('27 MB'),
+      ].each do |x|
+        x.number.should == 27
+        x.unit.should == 'MB'
+        x.bytes.should == 27 * 1048576
+        x.to_hash.should == { 'MB' => 27 }
+      end
+    end
+
+    it "2.3 MB" do
+      [
+        InfoUnit.new(2.3, 'MB'),
+        InfoUnit.new('2.3MB'),
+        InfoUnit.new('2.3 MB'),
+      ].each do |x|
+        x.number.should == 2.3
+        x.unit.should == 'MB'
+        x.bytes.should == 2.3 * 1048576
+        x.to_hash.should == { 'MB' => 2.3 }
+      end
+    end
+
+    it "1.01 KB" do
+      [
+        InfoUnit.new(1.01, 'KB'),
+        InfoUnit.new('1.01KB'),
+        InfoUnit.new('1.01 KB'),
+      ].each do |x|
+        x.number.should == 1.01
+        x.unit.should == 'KB'
+        x.bytes.should == 1.01 * 1024
+        x.to_hash.should == { 'KB' => 1.01 }
+      end
+    end
+
+    it "45" do
+      x = InfoUnit.new('45')
+      x.number.should == nil
+      x.unit.should == nil
+      x.bytes.should == nil
+      x.to_hash.should == {}
+    end
+
+    it "45 sheep" do
+      x = InfoUnit.new('45 sheep')
+      x.number.should == nil
+      x.unit.should == nil
+      x.bytes.should == nil
+      x.to_hash.should == {}
+    end
+  
   end
+
 end
